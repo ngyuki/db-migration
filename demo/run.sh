a702c90b812939ce9211f2aebd294747eadf9615
ryunosuke <ryunosuke.arima@gmail.com>
2015-04-02 20:26:49 +0900

    fix demo
    確認しづらいので cd するように変更

--- demo/run.sh
+++ demo/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEMODIR=$(dirname $(readlink -f $0))
+cd $(dirname $(dirname $(readlink -f $0)))
 
 function techo {
   echo -e "\e[0;42m$*\e[0m"
@@ -10,10 +10,10 @@ techo "create target database schema."
 mysql -e "DROP DATABASE IF EXISTS test_demo_migration;CREATE DATABASE test_demo_migration;"
 
 techo "import current database definitation. (imitate running database)"
-vendor/bin/doctrine-dbal dbal:import $DEMODIR/current/*
+vendor/bin/doctrine-dbal dbal:import demo/current/*
 
 techo "migrate latest database definitation. (exe no-interaction)"
-vendor/bin/doctrine-dbal dbal:migrate $DEMODIR/latest/* --no-interaction -v -k
+vendor/bin/doctrine-dbal dbal:migrate demo/latest/* --no-interaction -v -k
 
 techo "confirm no diff"
-vendor/bin/doctrine-dbal dbal:migrate $DEMODIR/latest/* --check
+vendor/bin/doctrine-dbal dbal:migrate demo/latest/* --check
