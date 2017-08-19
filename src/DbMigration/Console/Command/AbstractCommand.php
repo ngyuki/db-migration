24ce4d9f65e5665f0b2ec641656f738802902996
ryunosuke <ryunosuke.arima@gmail.com>
2017-08-19 18:10:23 +0900

    implemant AbstractCommand
    - コマンドの共通部分（Input/Output）を括りだし
    - confirm メソッドの追加

---
+++ src/DbMigration/Console/Command/AbstractCommand.php
@@ -0,0 +1,44 @@
+<?php
+namespace ryunosuke\DbMigration\Console\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+abstract class AbstractCommand extends Command
+{
+    /** @var QuestionHelper */
+    private $questionHelper;
+
+    /** @var InputInterface */
+    protected $input;
+
+    /** @var OutputInterface */
+    protected $output;
+
+    public function getQuestionHelper()
+    {
+        return $this->questionHelper ?: $this->questionHelper = new QuestionHelper();
+    }
+
+    protected function setInputOutput(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+    }
+
+    protected function confirm($message, $default = true)
+    {
+        $autoyes = $this->input->getOption('check') || $this->input->getOption('no-interaction');
+        if ($autoyes) {
+            return true;
+        }
+
+        $yesno = $default ? 'Y/n' : 'y/N';
+        $question = new Question("<question>{$message}[{$yesno}]:</question>", $default ? 'y' : 'n');
+
+        return 'y' === strtolower($this->getQuestionHelper()->doAsk($this->output, $question));
+    }
+}
